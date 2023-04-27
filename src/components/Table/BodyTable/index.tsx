e54ea84c10e569fa33353aac3988f49dbb78425a
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2023-04-26 19:39:31 -0600

    tabn

--- src/components/Table/BodyTable/index.tsx
+++ src/components/Table/BodyTable/index.tsx
@@ -19,59 +19,111 @@ type Props = {
   data: Data[];
 };
 
+type ColumnName = {
+  name:
+    | string
+    | "id"
+    | "comercio"
+    | "cuit"
+    | "concepto_1"
+    | "concepto_2"
+    | "concepto_3"
+    | "concepto_4"
+    | "concepto_5"
+    | "concepto_6"
+    | "balance_actual"
+    | "activo"
+    | "ultima_venta";
+};
+
+interface ColumnsGeneric {
+  name: string;
+  label: string;
+}
+
 const BodyTable: React.FC<Props> = ({ data }: Props) => {
+  const columnns: ColumnsGeneric[] = [
+    {
+      name: "id",
+      label: "ID",
+    },
+    {
+      name: "comercio",
+      label: "Comercio",
+    },
+    {
+      name: "cuit",
+      label: "CUIT",
+    },
+    {
+      name: "concepto_1",
+      label: "Concepto 1",
+    },
+    {
+      name: "concepto_2",
+      label: "Concepto 2",
+    },
+    {
+      name: "concepto_3",
+      label: "Concepto 3",
+    },
+    {
+      name: "concepto_4",
+      label: "Concepto 4",
+    },
+    {
+      name: "concepto_5",
+      label: "Concepto 5",
+    },
+    {
+      name: "concepto_6",
+      label: "Concepto 6",
+    },
+    {
+      name: "balance_actual",
+      label: "Balance actual",
+    },
+    {
+      name: "activo",
+      label: "Estatus",
+    },
+    {
+      name: "ultima_venta",
+      label: "Última venta",
+    },
+  ];
+
   return (
     <tbody className="bg-white divide-y divide-gray-200 text-white">
-      {data.map((item: Data) => (
+      {data.map((item: any) => (
         <tr
           key={item.id}
           className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
         >
-          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-white-900">
-            {item.id}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-white-900">
-            {item.comercio}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-white-500">
-            {item.cuit}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-white-500">
-            {item.balance_actual}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-white-500">
-            {item.concepto_1}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-white-500">
-            {item.concepto_2}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-white-500">
-            {item.concepto_3}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-white-500">
-            {item.concepto_4}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-white-500">
-            {item.concepto_5}
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-white-500">
-            {item.concepto_6}
-          </td>
-          {/* If active is true, set green if not red */}
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            <span
-              className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-                item.activo
-                  ? "bg-green-100 text-green-800"
-                  : "bg-red-100 text-red-800"
-              }`}
-            >
-              {item.activo ? "Activo" : "Inactivo"}
-            </span>
-          </td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            {item.ultima_venta}
-          </td>
+          {columnns.map((column) => {
+            const columnName = column.name;
+            if (columnName != "activo") {
+              return (
+                <td key={String(column.name)} className="px-4 py-3">
+                  {item[columnName.toString()]}
+                </td>
+              );
+            } else {
+              return (
+                <td key={String(column.name)} className="px-4 py-3">
+                  {item[columnName.toString()] ? (
+                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                      Activo
+                    </span>
+                  ) : (
+                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                      Inactivo
+                    </span>
+                  )}
+                </td>
+              );
+            }
+          })}
         </tr>
       ))}
     </tbody>
